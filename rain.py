67d0755a6ba714f4f8d6db2b726233168c771e6e
hamza14596 <hmzuuu87@gmail.com>
2025-07-06 22:51:49 +0500

    created a window, initialized everything necesasar

--- rain.py
+++ rain.py
@@ -1,3 +1,28 @@
 import pygame
-import sys 
 
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("Outer Heroes")
+
+playerFace = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+
+
+
+def player():
+    screen.blit(playerFace, (playerX,playerY))
+
+running = True 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0,0,0))
+    player()
+    
+    pygame.display.update()
+    
