cf2e60d9c36f1795b3c4f7ac99ea9e0de56677d8
hamza14596 <hmzuuu87@gmail.com>
2025-07-17 15:39:23 +0500

    initialized the window adn added a moveable player

--- rain.py
+++ rain.py
@@ -12,17 +12,31 @@ playerY = 480
 
 
 
-def player():
-    screen.blit(playerFace, (playerX,playerY))
+def player(x,y):
+    screen.blit(playerFace, (x,y))
 
 running = True 
 while running:
+    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        playerX_change = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT :
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT :
+                playerX_change = 0
+       
 
-    screen.fill((0,0,0))
-    player()
+    playerX += playerX_change
     
+    if playerX <=0 :
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+    player(playerX,playerY)
     pygame.display.update()
-    
