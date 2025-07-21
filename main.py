4704d8cf17f39e11c3d045bfed547bef65825ba0
hamza <hmzuuu87@gmail.com>
2025-07-21 11:50:11 +0500

    Wrapped up everything left, created player collsion detection with displayable text and score

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
+from pygame import mixer
 import random
 import math
 
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+mixer.init()
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Outer Heroes")
+
 background = pygame.image.load("bgi.png")
+mixer.music.load('AttackOfTheKillerQueen.wav')
+mixer.music.set_volume(0.5)
+mixer.music.play(-1)
 
 playerFace = pygame.image.load('spaceship.png')
 playerX = 370
@@ -31,7 +37,20 @@ bulletY = 480
 bulletY_change = 3
 bulletFlag = "Ready"
 
-score = 0
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+ScoreX = 15
+ScoreY = 15
+
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
 
 def player(x, y):
     screen.blit(playerFace, (x, y))
@@ -42,16 +61,18 @@ def ghost(x, y):
 def bullet_fire(x, y):
     global bulletFlag
     bulletFlag = "Fire"
-    screen.blit(bulletFace, (x, y + 10))
+    screen.blit(bulletFace, (x + 10, y + 10))
 
 def is_collision(ghostX, ghostY, bulletX, bulletY):
     distance = math.sqrt((math.pow(ghostX - bulletX, 2)) + (math.pow(ghostY - bulletY, 2)))
-    if distance < 27:
-        return True
-    else:
-        return False
+    return distance < 27
+
+explosion_sound = mixer.Sound('deltarune-explosionn.wav')
+explosion_sound.set_volume(0.5)
 
 running = True
+game_over = False
+
 while running:
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
@@ -60,56 +81,67 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -0.7
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 0.7
-            if event.key == pygame.K_UP:
-                if bulletFlag == "Ready":
-                    bulletX = playerX
-                    bullet_fire(bulletX, bulletY)
+        if not game_over:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -0.7
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 0.7
+                if event.key == pygame.K_UP:
+                    if bulletFlag == "Ready":
+                        bullet_sound = mixer.Sound('laser.wav')
+                        bullet_sound.set_volume(0.2)
+                        bullet_sound.play()
+                        bulletX = playerX
+                        bullet_fire(bulletX, bulletY)
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
 
-    playerX += playerX_change
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
-        playerX = 736
+    if not game_over:
+        playerX += playerX_change
+        playerX = max(0, min(playerX, 736))
 
-    for i in range(num_of_ghosts):
-        ghostX[i] += ghostX_change[i]
-        if ghostX[i] >= 736:
-            ghostX_change[i] = -0.5
-            ghostY[i] += ghostY_change[i]
-        elif ghostX[i] <= 0:
-            ghostX_change[i] = 0.5
-            ghostY[i] += ghostY_change[i]
+        for i in range(num_of_ghosts):
+            if ghostY[i] + 64 >= playerY and ghostY[i] <= playerY + 64 and ghostX[i] < playerX + 64 and ghostX[i] + 64 > playerX:
 
-        collision = is_collision(ghostX[i], ghostY[i], bulletX, bulletY)
-        if collision:
+                for j in range(num_of_ghosts):
+                    ghostY[j] = 2000
+                game_over = True
+                explosion_sound.play()
+                break
+
+            ghostX[i] += ghostX_change[i]
+            if ghostX[i] >= 736:
+                ghostX_change[i] = -0.5
+                ghostY[i] += ghostY_change[i]
+            elif ghostX[i] <= 0:
+                ghostX_change[i] = 0.5
+                ghostY[i] += ghostY_change[i]
+
+            collision = is_collision(ghostX[i], ghostY[i], bulletX, bulletY)
+            if collision:
+                bulletY = 480
+                bulletFlag = "Ready"
+                score_value += 1
+                ghostX[i] = random.randint(0, 736)
+                ghostY[i] = random.randint(50, 150)
+
+            ghost(ghostX[i], ghostY[i])
+
+        if bulletY <= 0:
             bulletY = 480
             bulletFlag = "Ready"
-            score += 1
-            print("Score:", score)
-            ghostX[i] = random.randint(0, 736)
-            ghostY[i] = random.randint(50, 150)
 
-        ghost(ghostX[i], ghostY[i])
+        if bulletFlag == "Fire":
+            bullet_fire(bulletX, bulletY)
+            bulletY -= bulletY_change
 
-    if bulletY <= 0:
-        bulletY = 480
-        bulletFlag = "Ready"
+        player(playerX, playerY)
+        show_score(ScoreX, ScoreY)
 
-    if bulletFlag == "Fire":
-        bullet_fire(bulletX, bulletY)
-        bulletY -= bulletY_change
+    else:
+        game_over_text()
 
-    player(playerX, playerY)
     pygame.display.update()
-
-   
-    
